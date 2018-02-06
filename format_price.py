b25f9531a616b5a31163a8f024d1733f5c929fe9
igorzakhar <igorzakhar@gmail.com>
2018-02-06 12:23:14 +0300

    Adds convert_to_decimal and format_price function

--- format_price.py
+++ format_price.py
@@ -1,5 +1,23 @@
+import sys
+from decimal import Decimal, InvalidOperation
+
+
+def convert_to_decimal(price):
+    try:
+        return Decimal(price).quantize(Decimal('.00'))
+    except InvalidOperation as err:
+        return err
+
+
 def format_price(price):
-    pass
+    formatted_price = '{:,}'.format(price.normalize()).replace(',', ' ')
+    return formatted_price
+
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        price = convert_to_decimal(sys.argv[1])
+        if isinstance(price, InvalidOperation):
+            print("Incorrect value")
+        else:
+            print(format_price(price))
